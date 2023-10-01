0225ba65bb19e6df2e62f31363efb5d01371c081
Yuefeng Wang <aaadoki@gmail.com>
2023-10-01 19:22:35 -0400

    change the name

--- chatdata.py
+++ chatdata.py
@@ -3,11 +3,14 @@ import openai
 import sys
 sys.path.append('../..')
 
+import constants
+
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
 from langchain.document_loaders import PyPDFLoader
-loader = PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf")
+loader = PyPDFLoader("docs\LLXZ.pdf")
 pages = loader.load()
+len(pages)
